d40a324154b158f5cbca5ba0ffcd076f4cd31a34
Sergio Lopez <slp@sinrega.org>
2014-02-04 10:33:09 +0100

    Cleanup leftovers on LocationServiceWrapper.

--- TMessagesProj/src/standard/java/org/telegram/messenger/LocationServiceWrapper.java
+++ TMessagesProj/src/standard/java/org/telegram/messenger/LocationServiceWrapper.java
@@ -12,35 +12,11 @@ import org.telegram.ui.ApplicationLoader;
 import org.telegram.ui.LocationActivity;
 
 public class LocationServiceWrapper {
-    private ApplicationActivity parentActivity;
-
-//    public LocationServiceWrapper(ApplicationActivity activity) {
-//        parentActivity = activity;
-//    }
-
     public static boolean isGoogleMapsInstalled() {
         try {
             ApplicationInfo info = ApplicationLoader.applicationContext.getPackageManager().getApplicationInfo("com.google.android.apps.maps", 0 );
             return true;
         } catch(PackageManager.NameNotFoundException e) {
-//            AlertDialog.Builder builder = new AlertDialog.Builder(parentActivity);
-//            builder.setMessage("Install Google Maps?");
-//            builder.setCancelable(true);
-//            builder.setPositiveButton(getStringEntry(R.string.OK), new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialogInterface, int i) {
-//                    try {
-//                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.google.android.apps.maps"));
-//                        startActivity(intent);
-//                    } catch (Exception e) {
-//                        FileLog.e("tmessages", e);
-//                    }
-//                }
-//            });
-//            builder.setNegativeButton(R.string.Cancel, null);
-//            visibleDialog = builder.create();
-//            visibleDialog.setCanceledOnTouchOutside(true);
-//            visibleDialog.show();
             return false;
         }
     }
