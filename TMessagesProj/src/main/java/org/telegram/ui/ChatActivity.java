23db9e5b399dbb19fc96bb4e08d8b866f40afafa
23rd <23rd@vivaldi.net>
2018-10-22 02:46:19 +0300

    Fixed "Delete for everyone". e77347331e0b7599e9af34a44395379c7d1c36bf

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -10247,7 +10247,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     cell.setText(LocaleController.formatString("DeleteForUser", R.string.DeleteForUser, UserObject.getFirstName(currentUser)), "", false, false);
                 }
                 cell.setPadding(LocaleController.isRTL ? AndroidUtilities.dp(16) : AndroidUtilities.dp(8), 0, LocaleController.isRTL ? AndroidUtilities.dp(8) : AndroidUtilities.dp(16), 0);
-                cell.setChecked(true, true); // Mark "delete for user" as default.
+                deleteForAll[0] = true;
+                cell.setChecked(deleteForAll[0], true); // Mark "delete for user" as default.
                 frameLayout.addView(cell, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, 48, Gravity.TOP | Gravity.LEFT, 0, 0, 0, 0));
                 cell.setOnClickListener(v -> {
                     CheckBoxCell cell12 = (CheckBoxCell) v;
@@ -10299,7 +10300,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     cell.setText(LocaleController.formatString("DeleteForUser", R.string.DeleteForUser, UserObject.getFirstName(currentUser)), "", false, false);
                 }
                 cell.setPadding(LocaleController.isRTL ? AndroidUtilities.dp(16) : AndroidUtilities.dp(8), 0, LocaleController.isRTL ? AndroidUtilities.dp(8) : AndroidUtilities.dp(16), 0);
-                cell.setChecked(true, true); // Mark "delete for user" as default.
+                deleteForAll[0] = true;
+                cell.setChecked(deleteForAll[0], true); // Mark "delete for user" as default
                 frameLayout.addView(cell, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, 48, Gravity.TOP | Gravity.LEFT, 0, 0, 0, 0));
                 cell.setOnClickListener(v -> {
                     CheckBoxCell cell1 = (CheckBoxCell) v;
