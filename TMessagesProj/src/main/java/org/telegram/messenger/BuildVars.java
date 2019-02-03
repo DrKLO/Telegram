7c7f682c29bab4a7c9314c507f277f16cf32fc29
23rd <23rd@vivaldi.net>
2019-02-03 11:54:47 +0300

    Github CI.
     - Release.keystore.
     - Deploy apk to Artifacts.
     - Disable debug logs by default.
     - Update .gitignore.
     - Create separated fork sub-project.

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -40,8 +40,17 @@ public class BuildVars {
         if (ApplicationLoader.applicationContext != null) {
             SharedPreferences sharedPreferences = ApplicationLoader.applicationContext.getSharedPreferences("systemConfig", Context.MODE_PRIVATE);
             LOGS_ENABLED = DEBUG_VERSION || sharedPreferences.getBoolean("logsEnabled", DEBUG_VERSION);
+            LOGS_ENABLED = sharedPreferences.getBoolean("logsEnabled", false);
         }
+
+        BUILD_VERSION_STRING = BuildConfig.VERSION_NAME;
+        APP_ID = BuildConfig.APP_ID;
+        APP_HASH = BuildConfig.APP_HASH;
+        PLAYSTORE_APP_URL = "";
+        DEBUG_VERSION = true;
+        CHECK_UPDATES = (BuildConfig.CHECK_UPDATES != 0);
     }
+    public static int USER_ID_OWNER = BuildConfig.USER_ID_OWNER;
 
     public static boolean useInvoiceBilling() {
         return true;
