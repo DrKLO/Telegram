deb9a4fcb86c833a5c47134212bd4469b973b9a3
Zac <49862878+commitZac@users.noreply.github.com>
2019-05-28 12:31:25 +0300

    Redundant special case
    If the length of colours is 7, the IDs from 0 to 6 will return 0 to 6 respectively without the need for a special case.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/AvatarDrawable.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/AvatarDrawable.java
@@ -85,9 +85,6 @@ public class AvatarDrawable extends Drawable {
     }
 
     public static int getColorIndex(int id) {
-        if (id >= 0 && id < 7) {
-            return id;
-        }
         return Math.abs(id % Theme.keys_avatar_background.length);
     }
 
