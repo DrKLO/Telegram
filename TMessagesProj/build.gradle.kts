2ff763cd8e1c913176b051677444cc697e404203
Mikhail <priamm@mail.ru>
2021-02-12 02:13:49 +0700

    Fix

--- TMessagesProj/build.gradle.kts
+++ TMessagesProj/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 repositories {
@@ -318,6 +319,3 @@ android {
     }
 }
 
-plugins {
-    id("com.google.gms.google-services")
-}
