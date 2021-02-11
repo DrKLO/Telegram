f3a8dc99ee270cef4252ed9b6c124a6ddacb422a
wrwrabbit <76569803+wrwrabbit@users.noreply.github.com>
2021-02-11 10:17:17 +0300

    Update UserConfig.java
    increase MAX_ACCOUNT_COUNT = 5

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 public class UserConfig extends BaseController {
 
     public static int selectedAccount;
-    public final static int MAX_ACCOUNT_COUNT = 3;
+    public final static int MAX_ACCOUNT_COUNT = 5;
 
     private final Object sync = new Object();
     private boolean configLoaded;
