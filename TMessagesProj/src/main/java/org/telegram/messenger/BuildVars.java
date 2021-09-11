02edaec2c692db41aaaeda9b3aaa89cc0c0ff1bd
Younes-L <Younes-L@users.noreply.github.com>
2021-09-11 10:55:33 +0100

    add telegram.org/android URL to BuildVars

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -28,6 +28,7 @@ public class BuildVars {
     //
     public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
+    public static String DIRECT_APP_URL = "https://telegram.org/android";
 
     static {
         if (ApplicationLoader.applicationContext != null) {
