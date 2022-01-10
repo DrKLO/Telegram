0adeac98f0ff4fe7fcae49a059300cebdf3ce7a5
Nikita S <nikita@saraeff.net>
2022-01-10 17:37:46 +0100

    fix to count ads views While we load ADS we do not count views due they aren't displayed. This is normal behavior when you simply do not scrolled till the ads. This is our old behavior. Now, to ensure that we are "like a virg.. vanilla client" we will call the servers, like "hey! we're displayed ads!".
    Normally `getSponsoredMessages` should return only sponsored messages. But a sponsored message is a simple message (just bit more fields are used). So we will check it anyway maybe it contain some correct stuff which will be displayed (don't have `sponsor` mark) and we will display these messages (due they aren't contain ads).
    
    So shortly: we load ads, we don't display ads, we say "ads is displayed"
    
    (cherry picked from commit 71746f3e45fc97eb9329eac08bdcd6000334f1ac)
    (cherry picked from commit e49b9aad15a4b88b702d79c33489b20591ff77c7)

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -16985,6 +16985,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         if (arrayList == null) {
             return;
         }
+        ArrayList<MessageObject> arrayList2 = new ArrayList<>();
         for (int i = 0; i < arrayList.size(); i++) {
             MessageObject messageObject = arrayList.get(i);
             messageObject.resetLayout();
@@ -16993,11 +16994,18 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
             if (messageObject.sponsoredChannelPost != 0) {
                 messageId = messageObject.sponsoredChannelPost;
             }
-            getMessagesController().ensureMessagesLoaded(dialogId, messageId, null);
-
+            if (!messageObject.isSponsored()) {
+                getMessagesController().ensureMessagesLoaded(dialogId, messageId, null);
+                arrayList2.add(messageObject);
+            } else {
+                markSponsoredAsRead(messageObject);
+            }
+//            getMessagesController().ensureMessagesLoaded(dialogId, messageId, null);
         }
         sponsoredMessagesAdded = true;
-        processNewMessages(arrayList);
+        if (arrayList2.isEmpty()) return;
+//        processNewMessages(arrayList);
+        processNewMessages(arrayList2);
     }
 
     private void checkGroupCallJoin(boolean fromServer) {
@@ -17925,6 +17933,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     }
 
     private int getSponsoredMessagesCount() {
+        if (true) return 0;
         int sponsoredMessagesCount = 0;
         while (sponsoredMessagesCount < messages.size()) {
             if (!messages.get(sponsoredMessagesCount).isSponsored()) {
