433b3e7c12da394147fafa4541100c4fc1adc88f
Nikita S <nikita@saraeff.net>
2022-02-02 15:42:34 +0100

    disable `restricted`

--- TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
@@ -19890,7 +19890,7 @@ public class TLRPC {
             bot_chat_history = (flags & 32768) != 0;
             bot_nochats = (flags & 65536) != 0;
             verified = (flags & 131072) != 0;
-            restricted = (flags & 262144) != 0;
+            restricted = false;
             min = (flags & 1048576) != 0;
             bot_inline_geo = (flags & 2097152) != 0;
             support = (flags & 8388608) != 0;
@@ -20021,7 +20021,7 @@ public class TLRPC {
             bot_chat_history = (flags & 32768) != 0;
             bot_nochats = (flags & 65536) != 0;
             verified = (flags & 131072) != 0;
-            restricted = (flags & 262144) != 0;
+            restricted = false;
             min = (flags & 1048576) != 0;
             bot_inline_geo = (flags & 2097152) != 0;
             support = (flags & 8388608) != 0;
@@ -20152,7 +20152,7 @@ public class TLRPC {
             bot_chat_history = (flags & 32768) != 0;
             bot_nochats = (flags & 65536) != 0;
             verified = (flags & 131072) != 0;
-            restricted = (flags & 262144) != 0;
+            restricted = false;
             min = (flags & 1048576) != 0;
             bot_inline_geo = (flags & 2097152) != 0;
             support = (flags & 8388608) != 0;
@@ -20468,7 +20468,7 @@ public class TLRPC {
             bot_chat_history = (flags & 32768) != 0;
             bot_nochats = (flags & 65536) != 0;
             verified = (flags & 131072) != 0;
-            restricted = (flags & 262144) != 0;
+            restricted = false;
             min = (flags & 1048576) != 0;
             bot_inline_geo = (flags & 2097152) != 0;
             id = stream.readInt32(exception);
@@ -38571,7 +38571,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             id = stream.readInt32(exception);
             access_hash = stream.readInt64(exception);
@@ -38643,7 +38643,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             scam = (flags & 524288) != 0;
@@ -38793,7 +38793,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             id = stream.readInt32(exception);
@@ -38925,7 +38925,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             scam = (flags & 524288) != 0;
@@ -39042,7 +39042,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             scam = (flags & 524288) != 0;
@@ -39157,7 +39157,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             id = stream.readInt32(exception);
@@ -39234,7 +39234,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             id = stream.readInt32(exception);
@@ -39306,7 +39306,7 @@ public class TLRPC {
             broadcast = (flags & 32) != 0;
             verified = (flags & 128) != 0;
             megagroup = (flags & 256) != 0;
-            restricted = (flags & 512) != 0;
+            restricted = false;
             signatures = (flags & 2048) != 0;
             min = (flags & 4096) != 0;
             id = stream.readInt32(exception);
