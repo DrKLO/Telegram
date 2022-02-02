ed2b0e12ef6b318495557994b46f43b9ca96ae3c
Nikita S <nikita@saraeff.net>
2022-02-02 15:45:51 +0100

    disable `noforwards`
    (cherry picked from commit cc84dbf9afdf1954ad61c8f36d0875e804b67c7b)
    (cherry picked from commit d51f087ab4c5a1202df3c8abe5c87357cedbc659)
    (cherry picked from commit cdb17d2e994e6ae91ac7d30f3ff844eb4ad57e2a)

--- TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
+++ TMessagesProj/src/main/java/org/telegram/tgnet/TLRPC.java
@@ -39607,7 +39607,7 @@ public class TLRPC {
             deactivated = (flags & 32) != 0;
             call_active = (flags & 8388608) != 0;
             call_not_empty = (flags & 16777216) != 0;
-            noforwards = (flags & 33554432) != 0;
+            noforwards = false;
             id = stream.readInt64(exception);
             title = stream.readString(exception);
             photo = ChatPhoto.TLdeserialize(stream, stream.readInt32(exception), exception);
@@ -39888,7 +39888,7 @@ public class TLRPC {
             call_not_empty = (flags & 16777216) != 0;
             fake = (flags & 33554432) != 0;
             gigagroup = (flags & 67108864) != 0;
-            noforwards = (flags & 134217728) != 0;
+            noforwards = false;
             join_to_send = (flags & 268435456) != 0;
             join_request = (flags & 536870912) != 0;
             id = stream.readInt64(exception);
@@ -55805,7 +55805,7 @@ public class TLRPC {
             legacy = (flags & 524288) != 0;
             edit_hide = false;
             pinned = (flags & 16777216) != 0;
-            noforwards = (flags & 67108864) != 0;
+            noforwards = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = Peer.TLdeserialize(stream, stream.readInt32(exception), exception);
@@ -55988,7 +55988,7 @@ public class TLRPC {
             legacy = (flags & 524288) != 0;
             edit_hide = false;
             pinned = (flags & 16777216) != 0;
-            noforwards = (flags & 67108864) != 0;
+            noforwards = false;
             id = stream.readInt32(exception);
             if ((flags & 256) != 0) {
                 from_id = Peer.TLdeserialize(stream, stream.readInt32(exception), exception);
