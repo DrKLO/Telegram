47e9e2fc7d988016f27144f94f4a8890f7c942c1
Philippe Laporte <philippe.laporte@productscience.ai>
2022-12-12 17:07:33 -0500

    DispatchQueuePool code in, commented out for now, reported equivalent by Jordan

--- TMessagesProj/src/main/java/org/telegram/messenger/MultiDispatchQueue.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MultiDispatchQueue.java
@@ -3,6 +3,9 @@ package org.telegram.messenger;
 // or use a ThreadPoolExecutor like in DispatchQueuePriority
 public class MultiDispatchQueue extends Thread {
 
+    // private final DispatchQueuePool dispatchQueuePool;
+
+
     private final int numQueues;
     private final DispatchQueue[] queues;
     private int curQueue = 0;
@@ -12,6 +15,8 @@ public class MultiDispatchQueue extends Thread {
     public MultiDispatchQueue(String threadName, int concurrencyLevel) {
         super(threadName);
 
+        //   dispatchQueuePool = new DispatchQueuePool(concurrencyLevel);
+
         numQueues = concurrencyLevel;
 
         queues = new DispatchQueue[numQueues];
@@ -19,18 +24,24 @@ public class MultiDispatchQueue extends Thread {
         for(int i = 0; i < numQueues; i++) {
             queues[i] = new DispatchQueue(threadName + "-multi-" + i, true);
         }
+
     }
 
     public synchronized boolean postRunnable(Runnable runnable) {
         boolean ret = queues[curQueue].postRunnable(runnable);
         curQueue = (curQueue + 1) % numQueues;
         return ret;
+        // dispatchQueuePool.execute(runnable);
+        //  return true;
     }
 
+    // TODO
     public synchronized boolean postRunnable(Runnable runnable, long delay) {
         boolean ret = queues[curQueue].postRunnable(runnable, delay);
         curQueue = (curQueue + 1) % numQueues;
         return ret;
+        //  dispatchQueuePool.execute(runnable);
+        // return true;
     }
 
     public void cancelRunnable(Runnable runnable) {
