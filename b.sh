7ec0dd016ae09fce3cc841911e6307b8851f46ff
teamgram.io <wubenqi@gmail.com>
2023-02-02 19:18:11 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/Contents/Home:$JAVA_HOME"
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/Contents/Home"
 export PATH="/Applications/Android Studio.app/Contents/jre/Contents/Home/bin:$PATH"
