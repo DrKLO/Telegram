e038d00c04b726124e44a328da64f4855c109752
thermatk <thermatk@thermatk.com>
2023-03-05 23:28:31 +0100

    [TF][JNI] remove native deps to be replaced

--- TMessagesProj/jni/boringssl/util/fipstools/delocate/testdata/generic-FileDirectives/in.s
+++
@@ -1,6 +0,0 @@
-.file 10 "some/path/file.c" "file.c"
-.file 1000 "some/path/file2.c" "file2.c"
-.file 1001 "some/path/file_with_md5.c" "other_name.c" md5 0x5eba7844df6449a7f2fff1556fe7ba8d239f8e2f
-
-# An instruction is needed to satisfy the architecture auto-detection.
-        movq %rax, %rbx
