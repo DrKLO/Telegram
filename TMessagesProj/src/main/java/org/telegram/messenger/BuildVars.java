7daa682ba1fc0c640e5d4b40e2103223ee0935ce
Gex-dev69 <dgexion@outlook.com>
2023-04-12 00:05:16 +0300

    it actually compiles, Nice.

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -11,9 +11,10 @@ package org.telegram.messenger;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import com.android.billingclient.api.ProductDetails;
 
+import java.io.File;
 import java.util.Objects;
 
 public class BuildVars {
@@ -26,8 +27,11 @@ public class BuildVars {
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
     public static int BUILD_VERSION = 3252;
     public static String BUILD_VERSION_STRING = "9.5.8";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+
+    public static int APP_ID = Integer.parseInt(BuildConfig.APP_ID);
+
+   public static String APP_HASH = BuildConfig.APP_HASH;
+
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
     public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
