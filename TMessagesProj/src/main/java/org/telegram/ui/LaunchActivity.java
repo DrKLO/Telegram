e19ff132112cfc43956bc140fafcc7a04c5ce3f9
rezaei <86908332+rezaei7@users.noreply.github.com>
2023-12-12 14:39:34 +0330

    fix memory leak in LaunchActivity
    fix memory leak in LaunchActivity when onDestroy() method from LaunchActivity called but onFragmentDestroy() from DialogsActivity doesn't called and this causes a memory leak

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -5880,6 +5880,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         } catch (Exception e) {
             FileLog.e(e);
         }
+        clearFragments();
         super.onDestroy();
         onFinish();
         FloatingDebugController.onDestroy();
