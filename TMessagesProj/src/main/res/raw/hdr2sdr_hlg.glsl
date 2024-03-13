de47e6b2a0158ca339c425ba9c9c7989e8480741
Vadym Turchenko <turchenkovadim2000@gmail.com>
2024-03-13 22:26:37 +0200

    fix: Make colors brighter when converting HDR to SDR

--- TMessagesProj/src/main/res/raw/hdr2sdr_hlg.glsl
+++ TMessagesProj/src/main/res/raw/hdr2sdr_hlg.glsl
@@ -4,11 +4,23 @@ precision highp float;
 
 uniform samplerExternalOES sTexture;
 
-const float a = 0.17883277;
-const float b = 0.28466892;
-const float c = 0.55991073;
-vec3 HLG_EOTF(vec3 x) {
+vec3 HLG_OETF_inv(vec3 x) {
+    const float a = 0.17883277;
+    const float b = 0.28466892;
+    const float c = 0.55991073;
     return mix((x * x) / 3.0, (b + exp((x - c) / a)) / 12.0, step(0.5, x));
+}
+
+vec3 HLG_OOTF(vec3 x) {
+    const float HLG_gamma = 1.2;
+    const vec3 Lvec = vec3(0.2627, 0.6780, 0.0593);
+
+    float Y = dot(Lvec, x);
+    return pow(Y, HLG_gamma - 1.0) * x;
+}
+
+vec3 HLG_EOTF(vec3 x) {
+    return HLG_OOTF(HLG_OETF_inv(x));
 }
 
 vec3 sRGB_EOTF(vec3 x) {
@@ -20,13 +32,14 @@ vec3 sRGB_OETF(vec3 x) {
 }
 
 //HDR to SDR conversion method A described in ITU-R BT.2446-1
-const float L_HDR = 1000.0;
-const float rho_HDR = 1.0 + 32.0 * pow(L_HDR / 10000.0, 1.0 / 2.4);
-const float L_SDR = 100.0;
-const float rho_SDR = 1.0 + 32.0 * pow(L_SDR / 10000.0, 1.0 / 2.4);
-const vec3 Lvec = vec3(0.2627, 0.6780, 0.0593);
 vec3 BT2446_tone_mapping_A(vec3 rgb_BT2020_linear) {
-    vec3 rgb_prime = pow(rgb_BT2020_linear, vec3(1.0 / 2.4));
+    const float L_HDR = 1000.0;
+    const float rho_HDR = 1.0 + 32.0 * pow(L_HDR / 10000.0, 1.0 / 2.4);
+    const float L_SDR = 100.0;
+    const float rho_SDR = 1.0 + 32.0 * pow(L_SDR / 10000.0, 1.0 / 2.4);
+    const vec3 Lvec = vec3(0.2627, 0.6780, 0.0593);
+
+    vec3 rgb_prime = sRGB_OETF(rgb_BT2020_linear);
 
     float Y_prime = dot(Lvec, rgb_prime);
 
@@ -47,8 +60,9 @@ vec3 BT2446_tone_mapping_A(vec3 rgb_BT2020_linear) {
     return sRGB_EOTF(rgb_BT2020_prime_TMO);
 }
 
-const mat3 BT2020_to_BT709 = mat3(1.6605, -0.1246, -0.0182, -0.5876, 1.1329, -0.1006, -0.0728, -0.0083, 1.1187);
 vec4 transform(vec3 rgb_BT2020_hlg) {
+    const mat3 BT2020_to_BT709 = mat3(1.6605, -0.1246, -0.0182, -0.5876, 1.1329, -0.1006, -0.0728, -0.0083, 1.1187);
+
     vec3 rgb_BT2020_linear = HLG_EOTF(rgb_BT2020_hlg);
     vec3 rgb_BT2020_linear_TMO = BT2446_tone_mapping_A(rgb_BT2020_linear);
     vec3 rgb_BT709_linear = clamp(BT2020_to_BT709 * rgb_BT2020_linear_TMO, 0.0, 1.0);
--- TMessagesProj/src/main/res/raw/hdr2sdr_pq.glsl
+++ TMessagesProj/src/main/res/raw/hdr2sdr_pq.glsl
@@ -18,13 +18,14 @@ vec3 sRGB_OETF(vec3 x) {
 }
 
 //HDR to SDR conversion method A described in ITU-R BT.2446-1
-const float L_HDR = 1000.0;
-const float rho_HDR = 1.0 + 32.0 * pow(L_HDR / 10000.0, 1.0 / 2.4);
-const float L_SDR = 100.0;
-const float rho_SDR = 1.0 + 32.0 * pow(L_SDR / 10000.0, 1.0 / 2.4);
-const vec3 Lvec = vec3(0.2627, 0.6780, 0.0593);
 vec3 BT2446_tone_mapping_A(vec3 rgb_BT2020_linear) {
-    vec3 rgb_prime = pow(rgb_BT2020_linear, vec3(1.0 / 2.4));
+    const float L_HDR = 1000.0;
+    const float rho_HDR = 1.0 + 32.0 * pow(L_HDR / 10000.0, 1.0 / 2.4);
+    const float L_SDR = 100.0;
+    const float rho_SDR = 1.0 + 32.0 * pow(L_SDR / 10000.0, 1.0 / 2.4);
+    const vec3 Lvec = vec3(0.2627, 0.6780, 0.0593);
+
+    vec3 rgb_prime = sRGB_OETF(rgb_BT2020_linear);
 
     float Y_prime = dot(Lvec, rgb_prime);
 
@@ -45,8 +46,9 @@ vec3 BT2446_tone_mapping_A(vec3 rgb_BT2020_linear) {
     return sRGB_EOTF(rgb_BT2020_prime_TMO);
 }
 
-const mat3 BT2020_to_BT709 = mat3(1.6605, -0.1246, -0.0182, -0.5876, 1.1329, -0.1006, -0.0728, -0.0083, 1.1187);
 vec4 transform(vec3 rgb_BT2020_pq) {
+    const mat3 BT2020_to_BT709 = mat3(1.6605, -0.1246, -0.0182, -0.5876, 1.1329, -0.1006, -0.0728, -0.0083, 1.1187);
+
     vec3 rgb_BT2020_linear = PQ_EOTF(rgb_BT2020_pq);
     vec3 rgb_BT2020_linear_TMO = BT2446_tone_mapping_A(rgb_BT2020_linear);
     vec3 rgb_BT709_linear = clamp(BT2020_to_BT709 * rgb_BT2020_linear_TMO, 0.0, 1.0);
