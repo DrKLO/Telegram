648ad5ab9be82b159b609b8d26b8c86ff18602d2
AlexandrKozlovskiy <k.sasha1994@gmail.com>
2024-05-03 00:55:59 +0300

    fixed memory leak

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -6426,6 +6426,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
         } catch (Exception e) {
             FileLog.e(e);
         }
+    clearFragments();
         super.onDestroy();
         onFinish();
         FloatingDebugController.onDestroy();
