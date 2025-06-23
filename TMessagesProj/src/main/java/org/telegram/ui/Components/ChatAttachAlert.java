675e288a6652e99456e015b74489646585f15330
arsLan4k1390 <65973611+arsLan4k1390@users.noreply.github.com>
2025-06-23 22:05:13 +0500

    Fix freeze on attachments close

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlert.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlert.java
@@ -5708,6 +5708,7 @@ public class ChatAttachAlert extends BottomSheet implements NotificationCenter.N
         captionLimitBulletinShown = false;
         super.dismiss();
         allowPassConfirmationAlert = false;
+        removeFromRoot();
     }
 
     @Override
