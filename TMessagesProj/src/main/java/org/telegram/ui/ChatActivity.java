685ace8292d418511e7abfff531fd249e03e0c75
Yong Jie Wong <yjwong92@gmail.com>
2026-09-11 17:51:56 +0800

    Fix infinite loop in calculateHeightUntilOutOwner

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -24557,9 +24557,8 @@ public class ChatActivity extends BaseFragment implements
         int t = chatListView.getHeight();
         int b = 0;
 
-        int a = startRow;
         long groupId = 0;
-        while (true) {
+        for (int a = startRow; ; a++) {
             final View view = chatListView.findViewByPosition(a);
             final MessageObject messageObject;
             final int padding;
@@ -24636,8 +24635,6 @@ public class ChatActivity extends BaseFragment implements
                 }
                 groupId = msgGroupId;
             }
-
-            a++;
         }
 
         return Math.max(0, b - t);
